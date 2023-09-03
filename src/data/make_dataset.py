9067f45cda2336734132ccb18b35293000c60c32
Xavier <xavialex7@gmail.com>
2023-09-03 20:36:14 +0200

    Add saving functionality to make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from datasets import load_dataset
 from datasets import ClassLabel
 
@@ -10,25 +12,27 @@ LABEL_CONVERSION = {'dk': 'Danish',
                     'sv': 'Swedish'}
 
 
-#def id2label_fn(dataset):
-#    return dataset['train'].features["language"].int2str
-
-
 def create_labels(example, id2label_fn):
-    example['aaa'] = LABEL_CONVERSION.get(id2label_fn(example['language']))
+    example['language_id'] = LABEL_CONVERSION.get(id2label_fn(example['language']))
     return example
 
 
-def make_dataset():
+def make_dataset(save_path: str=None):
     dataset = load_dataset('strombergnlp/nordic_langid', '10k')
     id2label_fn = dataset['train'].features["language"].int2str
-    aa = dataset.map(create_labels, fn_kwargs={'id2label_fn': id2label_fn})
+    dataset = dataset.map(create_labels, fn_kwargs={'id2label_fn': id2label_fn})
 
-    aa = aa.cast_column('aaa', ClassLabel(names=list(set(LABEL_CONVERSION.values()))))
-    aa = aa.remove_columns('language')
-    aa = aa.rename_column('aaa', 'language')
+    dataset = dataset.cast_column('language_id', ClassLabel(
+        names=list(set(LABEL_CONVERSION.values()))))
+    dataset = dataset.remove_columns('language')
+    dataset = dataset.rename_column('language_id', 'language')
 
-    print('kk')
+    if save_path is not None:
+        Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+        dataset.save_to_disk(save_path)
+        print(f"Dataset saved in {save_path}")
+
+    return dataset
 
 
 def main():
---
+++ src/models/predict_model.py
@@ -0,0 +1,39 @@
+import torch
+from torch import Tensor
+
+from src.models.train_model import vocab, tokenizer, model
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+LABEL_TO_LANGUAGE = {0: 'Other', 
+                     1: 'Norwegian',
+                     2: 'Danish',
+                     3: 'Swedish'}
+
+
+def text_preprocessing(text: str) -> Tensor:
+    encoding = vocab(tokenizer(text))
+    max_len = 128
+
+    if len(encoding) < max_len:
+        encoding = encoding + [0] * (max_len - len(encoding))
+    else:
+        encoding = encoding[:max_len]
+
+    return torch.tensor(encoding).to(device)
+
+
+def main():
+    model_path  ='classification_model2.pth'
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
+
+    text = "This is a sample test"
+    encoding = text_preprocessing(text)
+    label = model(encoding).argmax(1)
+    print(f"Identified language for '{text}': {label}")
+
+if __name__ == '__main__':
+    main()
+    
